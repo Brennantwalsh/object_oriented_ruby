eac26f0e85663b2704cc998f2213c59340f7a114
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-22 21:04:08 -0500

    class

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,31 @@
-item_1 = {name: "pen", price: 5, color: "black"}
-item_1 = {name: "pencil", price: 7, color: "yellow"}
-item_1 = {:name => "eraser", :price => 10, :color => "blue"}
+# item_1 = {name: "pen", price: 5, color: "black"}
+# item_1 = {name: "pencil", price: 7, color: "yellow"}
+# item_1 = {"name" => "eraser", :price => 10, :color => "blue"}
+
+class Item
+  def initialize(hash)
+    @name = hash[:name]
+    @price = hash[:price]
+    @color = hash[:color]
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def color
+    @color
+  end
+
+  def info 
+    "#{@name} costs #{@price} and is #{@color}"
+  end
+end
+
+item_1 = Item.new({name: "pen", price: 5, color: "black"})
+puts item_1.info
+
